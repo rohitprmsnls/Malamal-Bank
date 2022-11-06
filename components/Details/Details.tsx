adf4b5037f576eeeeb07f4ce8a953cd206776587
Harshlions <harshalpa2211@gmail.com>
2022-11-06 16:05:21 +0530

    css changes

--- components/Details/Details.tsx
+++ components/Details/Details.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Button, Form, Input } from "antd";
 import s from "./Details.module.scss";
-import {  createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  updatePassword,
+  updateProfile,
+} from "firebase/auth";
 import { auth } from "@config/firebase";
 
 // import Link from "next/link";
@@ -23,7 +28,27 @@ const Details = () => {
     } catch (error: any) {
       console.log(error.message);
     }
+    // updateProfile(auth?.currentUser, {
+    //   displayName: "User",
+    // })
   };
+
+
+
+  // const resetPass = () => {
+  //   const user = auth.currentUser;
+  //   const newPassword = getASecureRandomPassword();
+  
+  //   updatePassword(user, newPassword)
+  //     .then(() => {
+  //       // Update successful.
+  //     })
+  //     .catch((error) => {
+  //       // An error ocurred
+  //       // ...
+  //     });
+
+  // }
 
 
 
@@ -36,61 +61,63 @@ const Details = () => {
 
   return (
     <div className={`${s.container}`}>
-    <div className={`${s.form}`}>
-      <h1>Enter Details Here</h1>
-      <Form>
-        <Form.Item
-          name="name"
-          rules={[
-            {
-              required: false,
-              message: "Enter your name",
-            },
-          ]}
-        >
-          <Input placeholder="NAME" className={`${s.input_data}`} />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          rules={[
-            {
-              required: true,
-              message: "Enter your Email ID",
-            },
-          ]}
-        >
-          <Input
-            placeholder="Email"
-            className={`${s.input_data}`}
-            type="email"
-            onChange={(e: any) => {
-              setRegisitorEmail(e.target.value);
-            }}
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Enter your password",
-            },
-          ]}
-        >
-          <Input
-            placeholder="password"
-            className={`${s.input_data}`}
-            onChange={(e: any) => {
-              setRegisitorPassword(e.target.value);
-            }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button onClick={register}>Registor</Button>
-        </Form.Item>
-      </Form>
+      <div className={`${s.form}`}>
+        <h1>Enter Details Here</h1>
+        <Form>
+          <Form.Item
+            name="name"
+            rules={[
+              {
+                required: false,
+                message: "Enter your name",
+              },
+            ]}
+          >
+            <Input placeholder="NAME" className={`${s.input_data}`} />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Enter your Email ID",
+              },
+            ]}
+          >
+            <Input
+              placeholder="Email"
+              className={`${s.input_data}`}
+              type="email"
+              onChange={(e: any) => {
+                setRegisitorEmail(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Enter your password",
+              },
+            ]}
+          >
+            <Input
+              placeholder="password"
+              className={`${s.input_data}`}
+              onChange={(e: any) => {
+                setRegisitorPassword(e.target.value);
+              }}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button onClick={register}
+            className='btnReg'
+            >Registor</Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
-  </div>
   );
 };
 
